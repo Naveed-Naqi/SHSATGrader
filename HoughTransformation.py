64332ba26fe1c807d17455fcd5d026b1deb1885d
Naveed Naqi <nnaqi5534@gmail.com>
2019-03-04 23:25:30 -0500

    Create the first branch to start the Hough Transformation experiment

---
+++ HoughTransformation.py
@@ -0,0 +1,25 @@
+#This program succesffuly reads in an image and finds all contours after converting to a binary image
+#The contours are then all drawn on the original image
+#Dev Notes: Make sure the circle is completely filled in and there are no stray marks on the page
+#Dev Notes: Make sure that the the pencil marks are inside the bubbles as much as possible
+#Dev Notes: Make sure that only ONE answer is selected
+
+import cv2
+import imutils
+from imutils import contours
+import numpy as np
+
+
+image = cv2.imread('test_image.png')                 #Reads the image
+
+cv2.imshow("original image", image)
+cv2.waitKey(0)                                          #Waits for button press before moving on
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)    #Converts the image to greyscale
+
+#Converts to a binary image by changing the background of the image to black and the foreground to white
+binary_image = cv2.threshold(gray_image, 0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cv2.imshow("Binary Image", binary_image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
